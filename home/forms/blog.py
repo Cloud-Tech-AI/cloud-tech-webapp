bf3a6d05f3bffcfdb69f04fd9538ef05c636f7b1
ishan-modi <ishan.modi@glib.ai>
2023-06-20 21:24:27 +0530

    bug fixes during unit testing

--- home/forms/blog.py
+++ home/forms/blog.py
@@ -61,6 +61,15 @@ class BlogCreateForm(forms.ModelForm):
             }
         ))
     
+    pub_date = forms.DateTimeField(
+        required=False,
+        widget=forms.DateTimeInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    
     class Meta:
         model = Blog
-        fields = ['title', 'sub_title','image', 'body', 'co_author', 'tags']
+        fields = ['title', 'sub_title','image', 'body', 'co_author', 'tags', 'pub_date']
