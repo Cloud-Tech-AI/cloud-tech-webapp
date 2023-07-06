aa4b56c28473d3dbdac5720bc42a43ae993cc257
ishan-modi <ishan.modi@glib.ai>
2023-07-06 20:05:59 +0530

    updates and minor fixes

--- cloudtech_project/settings.py
+++ cloudtech_project/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-39yux8xnm4d$aqwc&9!wn_hxz%1ki=1rivuibwj$f38u0gl(5s'
+SECRET_KEY = env.str('SECRET_KEY', default='Ishan123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=True)
 
 ALLOWED_HOSTS = [env.str('TENANT_USERS_DOMAIN')[1:]] if env.str('TENANT_USERS_DOMAIN', None) else ['*']
 
